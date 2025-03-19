ccc5c2c7064ff941d26ae2b390c83256624580b2
Sina Kuhestani <sinakuhestani@gmail.com>
2025-03-19 13:23:49 +0330

    1.1

--- src/Stock.php
+++ src/Stock.php
@@ -1,49 +1,93 @@
 <?php
 
-declare(strict_types= 1);
+/*
+ * This file is part of the PHPallas package.
+ *
+ * (c) Sina Kuhestani <sinakuhestani@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace PHPallas\Buffer;
 
 use PHPallas\Utilities\ArrayUtility;
 
-class Stock
+/**
+ * Summary of Stock
+ */
+final class Stock
 {
+    private const separator = ".";
+
     private static ? Stock $instance = NULL;
+
     private array $data = [];
+
     private array $helpers = [];
-    private function __construct ()
+
+    private function __construct()
     {
     }
-    public static function getInstance (): static
+
+    public static function getInstance(): static
     {
-        if ( TRUE === is_null (static::$instance)) {
-            static::$instance = new static ();
+        if (null === static::$instance) {
+            static::$instance = new static();
         }
         return static::$instance;
     }
-    public function get ( string $name ): mixed
+
+    public function get(string $name, string $scope = "main"): mixed
     {
-        return ArrayUtility::get ( $this -> data, $name );
+        return ArrayUtility::get($this->data, static::scopeKey($scope) . static::separator . $name);
     }
-    public function set ( string $name, mixed $value )
+
+    public function set(string $name, mixed $value, string $scope = "main")
     {
-        ArrayUtility::set ($this -> data, $name, $value );
+        ArrayUtility::set($this->data, static::scopeKey($scope) . static::separator . $name, $value);
     }
-    public function register ( string $name, callable $function )
+
+    public function register(string $name, callable $function, string $scope = "main")
     {
-        $this -> helpers [ $name ] = $function;
+        ArrayUtility::set($this->helpers, static::scopeKey($scope) . static::separator . $name, $function);
     }
-    public function unregister ( string $name ): void
+
+    // public function unregister(string $name, string $scope = "main"): void
+    // {
+    //     if (TRUE === isset($this->helpers[static::scopeKey($scope). static::separator . $name])) {
+    //         unset($this->helpers[static::scopeKey($scope) . static::separator .  $name]);
+    //     }
+    // }
+    public function __call($method, $args = []): mixed
     {
-        if ( TRUE === isset ( $this -> helpers [ $name ] ) ) {
-            unset ( $this -> helpers [ $name ] );
+        if (isset($args["scope"])) {
+            $scope = $args["scope"];
+            unset($args["scope"]);
         }
-    }
-    public function __call ( $method, $args = [] ): mixed
-    {
-        if ( TRUE === isset ( $this -> helpers [ $method ] ) ) {
-            return call_user_func ( $this -> helpers [ $method ], $args );
+        else if (isset($args[0])) {
+            $scope = $args[0];
+            unset($args[0]);
+        }
+        else {
+            $scope = "main";
+        }
+
+        if (TRUE === isset($this->helpers[static::scopeKey($scope) . static::separator . $method])) {
+            return call_user_func($this->helpers[static::scopeKey($scope) . static::separator . $method], $args);
         }
         return NULL;
+    }
+
+    public function clearAll(): void
+    {
+        $this->data = [];
+    }
+
+    private static function scopeKey (string $scope): string
+    {
+        return "scope_" . sha1($scope);
     }
 }
