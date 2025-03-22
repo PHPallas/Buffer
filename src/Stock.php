0b46858e2fb7e93d2ea6b70d0a60becff321a3fc
Sina Kuhestani <sinakuhestani@gmail.com>
2025-03-22 03:44:56 +0330

    remove incomplete feature

--- src/Stock.php
+++ src/Stock.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace PHPallas\Buffer;
 
 use PHPallas\Utilities\ArrayUtility;
+use PHPallas\Utilities\StringUtility;
 
 /**
  * Summary of Stock
@@ -22,11 +23,9 @@ final class Stock
 {
     private const separator = ".";
 
-    private static ? Stock $instance = NULL;
+    private static ?Stock $instance = NULL;
 
     private array $data = [];
-
-    private array $helpers = [];
 
     private function __construct()
     {
@@ -42,43 +41,19 @@ final class Stock
 
     public function get(string $name, string $scope = "main"): mixed
     {
-        return ArrayUtility::get($this->data, static::scopeKey($scope) . static::separator . $name);
+        return ArrayUtility::get(
+            $this->data, 
+            static::scopeKey($scope) . static::separator . $name
+        );
     }
 
     public function set(string $name, mixed $value, string $scope = "main")
     {
-        ArrayUtility::set($this->data, static::scopeKey($scope) . static::separator . $name, $value);
-    }
-
-    public function register(string $name, callable $function, string $scope = "main")
-    {
-        ArrayUtility::set($this->helpers, static::scopeKey($scope) . static::separator . $name, $function);
-    }
-
-    // public function unregister(string $name, string $scope = "main"): void
-    // {
-    //     if (TRUE === isset($this->helpers[static::scopeKey($scope). static::separator . $name])) {
-    //         unset($this->helpers[static::scopeKey($scope) . static::separator .  $name]);
-    //     }
-    // }
-    public function __call($method, $args = []): mixed
-    {
-        if (isset($args["scope"])) {
-            $scope = $args["scope"];
-            unset($args["scope"]);
-        }
-        else if (isset($args[0])) {
-            $scope = $args[0];
-            unset($args[0]);
-        }
-        else {
-            $scope = "main";
-        }
-
-        if (TRUE === isset($this->helpers[static::scopeKey($scope) . static::separator . $method])) {
-            return call_user_func($this->helpers[static::scopeKey($scope) . static::separator . $method], $args);
-        }
-        return NULL;
+        ArrayUtility::set(
+            $this->data, 
+            static::scopeKey($scope) . static::separator . $name, 
+            $value
+        );
     }
 
     public function clearAll(): void
@@ -86,7 +61,7 @@ final class Stock
         $this->data = [];
     }
 
-    private static function scopeKey (string $scope): string
+    private static function scopeKey(string $scope): string
     {
         return "scope_" . sha1($scope);
     }
